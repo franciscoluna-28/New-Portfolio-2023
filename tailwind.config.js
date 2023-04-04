b77756ef5223cad7a183f6e7c84b9727fed573c5
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-04 17:19:50 -0400

    basic tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainExtraDarkBlue: "#0B0C10",
+        mainDarkBlue: "#1F2833",
+        mainSilver: "#C5C6C7",
+        mainCyan: "#66FCF1",
+        mainDarkCyan: "#45A293",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
