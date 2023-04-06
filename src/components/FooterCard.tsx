ce0afa24c1459038a256616876a73bf4fb3ade2b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-06 16:46:41 -0400

    created footer card component

---
+++ src/components/FooterCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { FooterCardProps as Footer } from "../Interfaces/FooterCardProps";
+
+export default function FooterCard({socialLink, styleClass, icon}: Footer) {
+    return(
+        <React.Fragment>
+            <a href={socialLink}><button className={styleClass}>{icon}</button></a>
+        </React.Fragment>
+    )
+}
