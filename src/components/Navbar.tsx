d401c02919034d93a0ae82df2ba45b7e48c51b62
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-07 11:16:01 -0400

    added details to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
-import Logo from './Logo';
+import "../styles/navbar.css"
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,79 +12,92 @@ export default function Navbar() {
   }
 
   return (
-    <header className="bg-transparent p-8 lg:p-6">
-      <nav className="flex lg:justify-center">
-        <FaBars
-          className="text-white text-3xl lg:hidden"
-          onClick={toggleMenu}
-        />
-        <AnimatePresence>
-          {isMenuOpen && (
-            <motion.section
-              key="menu"
-              initial={{ opacity: 0, y: -20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              transition={{ duration: 0.3 }}
-              className="lg:hidden absolute top-16 left-0 right-0 z-50 bg-white"
-            >
-              <NavLink
-                className="block px-4 py-2 dark:text-mainSilver dark:hover:text-white text-gray-800 hover:bg-gray-200"
-                to="/"
-                onClick={toggleMenu}
-              >
-                Home
-              </NavLink>
-              <NavLink
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-                to="/about"
-                onClick={toggleMenu}
-              >
-                About
-              </NavLink>
-              <NavLink
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-                to="/projects"
-                onClick={toggleMenu}
-              >
-                Projects
-              </NavLink>
-              <NavLink
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-                to="/tech-stack"
-                onClick={toggleMenu}
-              >
-                Tech Stack
-              </NavLink>
-              <NavLink
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-                to="/contact"
-                onClick={toggleMenu}
-              >
-                Contact
-              </NavLink>
-            </motion.section>
-          )}
-        </AnimatePresence>
-        <section className="hidden lg:flex gap-20 font-medium">
-        <Logo/>
-          <NavLink className="dark:text-mainSilver dark:hover:text-white text-gray-800 border-l-4 p-2 duration-200 border-transparent dark:hover:border-l-mainCyan" to="/">
-            Home
-          </NavLink>
-          <NavLink className=" dark:text-mainSilver dark:hover:text-white text-gray-800 border-l-4 p-2 duration-200 border-transparent dark:hover:border-l-mainCyan" to="/about">
-            About
-          </NavLink>
-          <NavLink className=" dark:text-mainSilver dark:hover:text-white text-gray-800 border-l-4 p-2 duration-200 border-transparent dark:hover:border-l-mainCyan" to="/projects">
-            Projects
-          </NavLink>
-          <NavLink className=" dark:text-mainSilver dark:hover:text-white text-gray-800 border-l-4 p-2 duration-200 border-transparent dark:hover:border-l-mainCyan" to="/tech-stack">
-            Tech Stack
-          </NavLink>
-          <NavLink className=" dark:text-mainSilver dark:hover:text-white text-gray-800 border-l-4 p-2 duration-200 border-transparent dark:hover:border-l-mainCyan" to="/contact">
-            Contact
-          </NavLink>
-        </section>
-      </nav>
-    </header>
+    <>
+      <header className="bg-transparent p-8 lg:p-6">
+        <nav className="flex lg:justify-center">
+          <FaBars
+            className="bars"
+            onClick={toggleMenu}
+          />
+          <AnimatePresence>
+            {isMenuOpen && (
+              <>
+                <motion.div
+                  className="dark-bar-div"
+                  onClick={toggleMenu}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.5 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                />
+                <motion.section
+                  key="menu"
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.3 }}
+                  className="hamburguer-bar"
+                >
+                  <NavLink
+                    className="hamburguer-bar-link"
+                    to="/"
+                    onClick={toggleMenu}
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    className="hamburguer-bar-link"
+                    to="/about"
+                    onClick={toggleMenu}
+                  >
+                    About
+                  </NavLink>
+                  <NavLink
+                    className="hamburguer-bar-link"
+                    to="/projects"
+                    onClick={toggleMenu}
+                  >
+                    Projects
+                  </NavLink>
+                  <NavLink
+                    className="hamburguer-bar-link"
+                    to="/tech-stack"
+                    onClick={toggleMenu}
+                  >
+                    Tech Stack
+                  </NavLink>
+                  <NavLink
+                    className="hamburguer-bar-link"
+                    to="/contact"
+                    onClick={toggleMenu}
+                  >
+                    Contact
+                  </NavLink>
+                </motion.section>
+              </>
+            )}
+          </AnimatePresence>
+          <section className="hidden lg:flex gap-20 font-medium">
+
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className=" nav-link" to="/about">
+              About
+            </NavLink>
+            <NavLink className="nav-link" to="/projects">
+              Projects
+            </NavLink>
+            <NavLink className="nav-link" to="/tech-stack">
+              Tech Stack
+            </NavLink>
+            <NavLink className="nav-link" to="/contact">
+              Contact
+            </NavLink>
+          </section>
+
+        </nav>
+      </header>
+    </>
   );
 }
