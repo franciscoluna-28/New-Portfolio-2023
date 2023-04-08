aa4cf5bfe999958d824a54255a6837bcae0ff8d5
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-08 12:15:38 -0400

    create dark mode context

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeContextProps {
+    theme: Theme;
+    setTheme: (theme: Theme) => void;
+}
+
+interface ThemeProviderProps {
+    children: React.ReactNode;
+}
+
+export const ThemeContext = createContext<ThemeContextProps>({
+    theme: 'light',
+    setTheme: () => { },
+});
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+    const [theme, setTheme] = useState<Theme>(getInitialTheme);
+
+    function getInitialTheme(): Theme {
+        const storedTheme = localStorage.getItem('color-theme') as Theme | null;
+
+        if (storedTheme) {
+            return storedTheme;
+        }
+
+        const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        return isDarkMode ? 'dark' : 'light';
+    }
+
+    const rawSetTheme = (theme: Theme) => {
+        const root = window.document.documentElement;
+        const isDark = theme === 'dark';
+
+        root.classList.remove(isDark ? 'light' : 'dark');
+        root.classList.add(theme);
+
+        localStorage.setItem('color-theme', theme);
+    };
+
+    useEffect(() => {
+        rawSetTheme(theme);
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
