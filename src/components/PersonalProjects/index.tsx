df001bef7df7e3c7c199995367ef9b111e608398
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-17 11:42:00 -0400

    implemented functionality to filter projects

---
+++ src/components/PersonalProjects/index.tsx
@@ -0,0 +1,21 @@
+import { createdProjects } from "./data";
+import ProjectCard, { project } from "../ProjectCard";
+import { useContext } from 'react';
+import { MainTechnologyContext } from '../../context/MainTechnologyContext';
+
+export default function PersonalProjects() {
+  const { selectedTechnology } = useContext(MainTechnologyContext);
+
+  // Filtering projects according to the selected technology
+  const filteredProjects: Array<project> = selectedTechnology
+    ? createdProjects.filter((project) => project.mainTechnology === selectedTechnology)
+    : createdProjects;
+
+  return (
+    <div className="flex gap-8 justify-center flex-wrap">
+      {filteredProjects.map((project) => (
+        <ProjectCard {...project} />
+      ))}
+    </div>
+  );
+}
