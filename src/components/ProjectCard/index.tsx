b64a22bd41b96ef1f8391b5d39363d99988559a8
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-17 16:24:29 -0400

    worked on dark mode theme

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -29,26 +29,26 @@ export default function ProjectCard({ image, title, description, githubLink, dep
     const isValidNetlifyLink = netlifyLinkRegex.test(deploymentLink);
 
     return (
-        <div className="card w-auto max-w-2xl shadow-xl dark:bg-gradient-to-r dark:from-mainDarkBlue/50 dark:to-mainDarkBlue/30 hover:scale-105 duration-300">
+        <div className="card w-auto max-w-2xl shadow-xl dark:bg-gradient-to-r dark:from-mainDarkBlue/50 dark:to-mainDarkBlue/30 lg:hover:scale-105 duration-300">
             <figure><img className='' src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title dark:text-white text-black font-bol">
                     {title}
-                    <div className="badge dark:bg-mainCyan dark:text-mainExtraDarkBlue/70 bg-mainDarkCyan outline-none border-transparent text-white">{mainTechnology}</div>
+                    <div className="badge glow-main dark:bg-mainCyan dark:text-mainExtraDarkBlue/70 bg-mainDarkCyan outline-none border-transparent text-white">{mainTechnology}</div>
                 </h2>
-                <p className='my-2'>{description}</p>
+                <p className='my-2 leading-loose dark:text-mainSilver/50 text-mainExtraDarkBlue/50'>{description}</p>
                 <div className="card-actions justify-end">
                     {tags.map((tag) => (
                         <div className="badge badge-outline" key={tag}>{tag}</div>
                     ))}
                     {isValidGithubLink && (
                         <a href={githubLink} target="_blank" rel="noopener noreferrer">
-                            <FaGithub className='text-xl dark:text-white' />
+                            <FaGithub className='text-xl dark:text-white text-mainExtraDarkBlue/70' />
                         </a>
                     )}
                     {isValidNetlifyLink && (
                         <a href={deploymentLink} target="_blank" rel="noopener noreferrer">
-                            <SiNetlify className='text-xl dark:text-mainCyan' />
+                            <SiNetlify className='text-xl dark:text-mainCyan text-mainDarkCyan' />
                         </a>
                     )}
                 </div>
