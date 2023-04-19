078c62e4a8d65854802affec0aa0f6dca5a7f3ca
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-18 20:27:04 -0400

    Added QR-Code Project

--- src/components/PersonalProjects/data.ts
+++ src/components/PersonalProjects/data.ts
@@ -1,5 +1,6 @@
 import BlogProject from "../../assets/projects/blog.png"
 import WeatherAppProject from "../../assets/projects/weather-app.png"
+import QRCodeApp from "../../assets/projects/qr-code-app.png"
 import { project } from "../ProjectCard"
 
 export const createdProjects: Array<project> = [
@@ -20,6 +21,15 @@ export const createdProjects: Array<project> = [
         deploymentLink: "https://weather-app-tailwind-react.netlify.app",
         tags: ["Hooks", "TaiwlindCSS", "API"],
         mainTechnology: "React"
+    },
+    {
+        image: QRCodeApp,
+        title: "QR COode Generator",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at turpis odio. Ut sodales velit ac tincidunt pretium. Vestibulum ante eros, ultrices quis laoreet id, iaculis at ligula. Curabitur dapibus sapien mi. Donec in ullamcorper tortor, nec auctor elit. In at tellus libero. Morbi viverra, odio ut laoreet consequat, lacus quam dapibus ante, sed imperdiet ipsum purus et felis. Cras eu sollicitudin nisl, eget tincidunt augue. Nunc nec augue a velit vestibulum tincidunt vitae at magna.",
+        githubLink: "https://github.com/franciscoluna-28/QR-Code-Generator-Node-JS",
+        deploymentLink: "https://qr-code-application-node-js.onrender.com",
+        tags: ["NodeJS", "Express", "Middleware"],
+        mainTechnology: "NodeJS"
     }
 ]
 
