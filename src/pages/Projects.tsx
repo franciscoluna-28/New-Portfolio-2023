df0dec8865ba9424e7c18f2ac8b7c08ed7db089f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-18 20:26:18 -0400

    Added tiny section for a paragraph

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,10 +12,13 @@ export default function Projects() {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}>
         <section className='p-4 lg:p-4 flex items-center flex-col justify-center min-h-screen'>
+          <div className='flex items-center flex-col'>
           <h1 className="font-bold text-4xl flex text-left max-639:justify-center max-639:text-4xl main text-mainDarkCyan dark:text-white lg:text-5xl my-12 pl-3 relative dark:main-title">
             My Projects
           </h1>
+          <p className='text-center text-mainExtraDarkBlue/70 dark:text-mainSilver mb-4'>Here you are able to see my projects as a Front End Developer!</p>
           <FilterByMainTechnology/>
+          </div>
           <PersonalProjects />
         </section>
       </motion.div>
