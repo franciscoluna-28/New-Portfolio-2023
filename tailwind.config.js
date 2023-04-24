1bbfa4870ae9b621acd7bd21189b4d32f6791c25
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-24 18:50:21 -0400

    Project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
-module.exports = {
+/* eslint-disable no-undef */
+// tailwind.config.js
+export default {
   darkMode: 'class',
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundColor: {
         dark: '#0B0C10',
       },
       colors: {
-        mainExtraDarkBlue: "#0B0C10",
-        mainDarkBlue: "#1F2833",
-        mainSilver: "#C5C6C7",
-        mainCyan: "#66FCF1",
-        mainDarkCyan: "#45A293",
+        mainExtraDarkBlue: '#0B0C10',
+        mainDarkBlue: '#1F2833',
+        mainSilver: '#C5C6C7',
+        mainCyan: '#66FCF1',
+        mainDarkCyan: '#45A293',
       },
       fontFamily: {
-        Inter: ["Inter", "sans-serif"],
+        Inter: ['Inter', 'sans-serif'],
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   variants: {
     extend: {
       backgroundColor: ['dark'],
