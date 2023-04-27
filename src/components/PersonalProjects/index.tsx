235bf2c45bf4e84c8468e3a45160f9ba6a69a36f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-27 12:25:15 -0400

    Updated card style

--- src/components/PersonalProjects/index.tsx
+++ src/components/PersonalProjects/index.tsx
@@ -3,6 +3,7 @@ import ProjectCard, { project } from "../ProjectCard";
 import { useContext } from 'react';
 import { MainTechnologyContext } from '../../context/MainTechnologyContext';
 
+
 export default function PersonalProjects() {
   const { selectedTechnology } = useContext(MainTechnologyContext);
 
@@ -12,10 +13,27 @@ export default function PersonalProjects() {
     : createdProjects;
 
   return (
+    <>
+    <div className="flex flex-col justify-center items-center py-12">
+    <h3
+            className="font-bold text-4xl lex text-left max-639:justify-center max-639:text-4xl main
+     text-mainDarkCyan dark:text-white lg:text-5xl"
+          >
+            Projects
+          </h3>
+          <h4 className="text-mainDarkBlue text-2xl dark:text-mainCyan font-bold text-left 
+flex py-2 max-639:justify-center lg:text-3xl">
+            That I've created
+          </h4>
+          <p className="dark:text-mainSilver/80 md:text-xl">See what I'm able to do</p>
+          </div>
     <div className="flex gap-4 lg:gap-10 justify-center flex-wrap">
+      
       {filteredProjects.map((project) => (
         <ProjectCard {...project} />
       ))}
     </div>
+    </>
+
   );
 }
