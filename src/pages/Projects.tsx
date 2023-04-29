e637db322b82fe485816a9fb45d3e13f122f0590
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-29 16:19:18 -0400

    Changed copywriting

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,18 +9,19 @@ export default function Projects() {
   return (
     <>
     <MainTechnologyProvider>
-    <div className="flex flex-col justify-center items-center pb-4 pt-12">
+    <div className="flex flex-col justify-center items-center pb-4 pt-12" id='projects'>
     <h3
             className="font-bold text-4xl lex text-left max-639:justify-center max-639:text-4xl main
      text-mainDarkCyan dark:text-white lg:text-5xl"
           >
-            Projects
+            Finished
           </h3>
           <h4 className="text-mainDarkBlue text-2xl dark:text-mainCyan font-bold text-left 
 flex py-2 max-639:justify-center lg:text-3xl">
-            That I've created
+            Projects
           </h4>
-          <p className="dark:text-mainSilver/80 md:text-xl">See what I'm able to do</p>
+          <p className="dark:text-mainSilver/80 md:text-xl text-center">Showcase of my personal finished projects. I explain the
+          problem I wanted to solve and the technologies and concepts I applied</p>
           </div>
     <FilterByMainTechnology/>
     
