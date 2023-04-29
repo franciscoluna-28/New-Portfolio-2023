31797990dbc379813f1410da03c7599623b7962a
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-28 21:21:07 -0400

    Removed old components

--- src/components/PersonalProjects/index.tsx
+++ src/components/PersonalProjects/index.tsx
@@ -19,20 +19,8 @@ export default function PersonalProjects() {
       whileInView={{opacity: 1, x:0}}
       transition={{type: "spring", duration: 1, delay: .15}}
       >
-    <div className="flex flex-col justify-center items-center py-12">
-    <h3
-            className="font-bold text-4xl lex text-left max-639:justify-center max-639:text-4xl main
-     text-mainDarkCyan dark:text-white lg:text-5xl"
-          >
-            Projects
-          </h3>
-          <h4 className="text-mainDarkBlue text-2xl dark:text-mainCyan font-bold text-left 
-flex py-2 max-639:justify-center lg:text-3xl">
-            That I've created
-          </h4>
-          <p className="dark:text-mainSilver/80 md:text-xl">See what I'm able to do</p>
-          </div>
-    <div className="flex gap-4 lg:gap-10 justify-center flex-wrap">
+
+    <div className="flex gap-4 lg:gap-10 justify-center flex-wrap mt-6">
       
       {filteredProjects.map((project) => (
         <ProjectCard key={project.id} {...project} />
