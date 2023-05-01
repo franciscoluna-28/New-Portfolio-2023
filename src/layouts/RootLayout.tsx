4f39c434e201c89994f715ac29283aacb84012ef
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-01 17:00:33 -0400

    Started experimenting with the useRef

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,14 +1,24 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { useRef } from "react"
+
+
+
 
 export default function RootLayout(){
+      // Crea la referencia y especifica el tipo
+  const homeRef = useRef<HTMLDivElement>(null);
+
+
     return(
         <>
         <Navbar/>
-        <main>
+        <main ref={homeRef}>
         <Outlet/>
        {/*  <span className="flex py-4 justify-center dark:text-mainDarkBlue">© Copyright 2023 Francisco Luna</span> */}
         </main>
+        <Footer homeRef={homeRef}/>
         </>
     )
 }
