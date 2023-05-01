9555527494dc8bafc625048bfcc8fcbc48abc12b
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-01 17:02:46 -0400

    Added animation

--- src/components/PersonalProjects/index.tsx
+++ src/components/PersonalProjects/index.tsx
@@ -2,7 +2,7 @@ import { createdProjects } from "./data";
 import ProjectCard, { project } from "../ProjectCard";
 import { useContext } from "react";
 import { MainTechnologyContext } from "../../context/MainTechnologyContext";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function PersonalProjects() {
   const { selectedTechnology } = useContext(MainTechnologyContext);
@@ -14,18 +14,21 @@ export default function PersonalProjects() {
       )
     : createdProjects;
 
+
+
   return (
-    <motion.div
-      viewport={{ once: false }}
-      initial={{ opacity: 0, x: 50 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ type: "spring", duration: 1, delay: 0.15 }}
-    >
-      <div className="flex gap-4 lg:gap-10 justify-center flex-wrap mt-6">
-        {filteredProjects.map((project) => (
-          <ProjectCard key={project.id} {...project} />
+    <div className="m-auto w-full flex gap-4 lg:gap-10 justify-center flex-wrap mt-6">
+        {filteredProjects.map((project) => (          <motion.div
+            viewport={{ once: false }}
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", duration: 1, delay: 0.15 }}
+          >
+
+            <ProjectCard {...project} />
+            </motion.div>
         ))}
-      </div>
-    </motion.div>
+
+    </div>
   );
 }
