41ea8bd0984bfb06db01271624ca19ed31f883a4
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-03 11:18:01 -0400

    Added references for scrolling from the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,50 +1,104 @@
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { SiBuymeacoffee } from "react-icons/si";
-import { RefObject } from "react";
+import { useContext } from "react";
+import { RefContext } from "../../context/ScrollContext";
+import { ScrollToComponent } from "../../hooks/ScrollToComponent";
 
+export default function Footer() {
+  const {
+    homeRef,
+    aboutRef,
+    experienceRef,
+    projectsRef,
+    servicesRef,
+    portfolioRef,
+    contactRef,
+  } = useContext(RefContext);
+  const scrollToHomeRef = ScrollToComponent({ rootRef: homeRef });
+  const scrollToAboutRef = ScrollToComponent({ rootRef: aboutRef });
+  const scrollToExperienceRef = ScrollToComponent({ rootRef: experienceRef });
+  const scrollToProjectsRef = ScrollToComponent({ rootRef: projectsRef });
+  const scrollToServicesRef = ScrollToComponent({ rootRef: servicesRef });
+  const scrollToPortfolioRef = ScrollToComponent({ rootRef: portfolioRef });
+  const scrollToContactRef = ScrollToComponent({ rootRef: contactRef });
 
-type FooterProps = {
-  homeRef: RefObject<HTMLDivElement>;
-}
-
-export default function Footer({homeRef}: FooterProps) {
-
-  // No hay DRY aqui
-   const handleScroll = () => {
-    homeRef.current?.scrollIntoView({
-      behavior: "smooth"
-    });
-  };
-  
   return (
     <footer className="flex flex-col w-full h-auto shadow-xl bg-mainDarkCyan px-8 py-8 dark:bg-mainDarkBlue/50">
-
-<h5 className="text-white font-semibold text-2xl m-auto mb-4 hover:text-mainCyan duration-500 cursor-pointer">Francisco Luna</h5>
-
+      <h5 className="text-white font-semibold text-2xl m-auto mb-4 hover:text-mainCyan duration-500 cursor-pointer">
+        Francisco Luna
+      </h5>
 
       <ul className="flex flex-col m-auto md:flex-row gap-6 text-center dark:text-mainSilver text-base flex-wrap justify-center mb-6">
         <li>
-          <button className="dark:text-mainSilver dark:hover:text-white
-          dark:hover:underline underline-offset-2 decoration-mainCyan" onClick={handleScroll}>Home</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToHomeRef}
+          >
+            Home
+          </button>
         </li>
         <li>
-          <button>About</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToAboutRef}
+          >
+            About
+          </button>
         </li>
         <li>
-          <button>Experience</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToExperienceRef}
+          >
+            Experience
+          </button>
         </li>
         <li>
-          <button>Services</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToProjectsRef}
+          >
+            Projects
+          </button>
         </li>
         <li>
-          <button>Portfolio</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToServicesRef}
+          >
+            Services
+          </button>
         </li>
         <li>
-          <button>UI / UX Design</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToPortfolioRef}
+          >
+            Portfolio
+          </button>
         </li>
         <li>
-          <button>Contact</button>
+          <button
+            className="dark:text-mainSilver dark:hover:text-white
+          dark:hover:underline decoration-white 
+          underline-offset-2 dark:decoration-mainCyan text-white hover:underline"
+            onClick={scrollToContactRef}
+          >
+            Contact
+          </button>
         </li>
       </ul>
 
@@ -53,10 +107,10 @@ export default function Footer({homeRef}: FooterProps) {
           <li className="">
             <div
               className="dark:bg-mainDarkBlue cursor-pointer
-                    dark:hover:border-mainCyan dark:hover:bg-transparent bg-mainExtraDarkBlue flex justify-center 
-                    items-center border-transparent shadow-lg rounded-xl 
-                    w-12 h-12 border-2
-                    duration-300 dark:hover:!text-mainCyan hover:bg-transparent hover:border-white"
+              dark:hover:border-mainCyan dark:hover:bg-transparent text-mainDarkCyan flex justify-center 
+              items-center border-transparent shadow-lg rounded-xl 
+              w-12 h-12 border-2
+              duration-300 dark:hover:!text-mainCyan hover:border-white bg-white"
             >
               <FaTwitter className="dark:text-white text-lg dark:hover:text-mainCyan" />
             </div>
@@ -66,10 +120,10 @@ export default function Footer({homeRef}: FooterProps) {
           <li className="">
             <div
               className="dark:bg-mainDarkBlue cursor-pointer
-                    dark:hover:border-mainCyan dark:hover:bg-transparent bg-mainExtraDarkBlue flex justify-center 
-                    items-center border-transparent shadow-lg rounded-xl 
-                    w-12 h-12 border-2
-                    duration-300 dark:hover:!text-mainCyan hover:bg-transparent hover:border-white"
+              dark:hover:border-mainCyan dark:hover:bg-transparent text-mainDarkCyan flex justify-center 
+              items-center border-transparent shadow-lg rounded-xl 
+              w-12 h-12 border-2
+              duration-300 dark:hover:!text-mainCyan hover:border-white bg-white"
             >
               <FaInstagram className="dark:text-white text-lg dark:hover:text-mainCyan" />
             </div>
@@ -79,17 +133,19 @@ export default function Footer({homeRef}: FooterProps) {
           <li className="">
             <div
               className="dark:bg-mainDarkBlue cursor-pointer
-                    dark:hover:border-mainCyan dark:hover:bg-transparent bg-mainExtraDarkBlue flex justify-center 
+                    dark:hover:border-mainCyan dark:hover:bg-transparent text-mainDarkCyan flex justify-center 
                     items-center border-transparent shadow-lg rounded-xl 
                     w-12 h-12 border-2
-                    duration-300 dark:hover:!text-mainCyan hover:bg-transparent hover:border-white"
+                    duration-300 dark:hover:!text-mainCyan hover:border-white bg-white"
             >
               <SiBuymeacoffee className="dark:text-white text-lg dark:hover:text-mainCyan" />
             </div>
           </li>
         </a>
       </ul>
-      <span className="m-auto text-sm mt-4 text-center">© Francisco Luna All rights reserved 2023</span>
+      <span className="m-auto text-sm mt-4 text-center text-white">
+        © Francisco Luna All rights reserved 2023
+      </span>
     </footer>
   );
 }
