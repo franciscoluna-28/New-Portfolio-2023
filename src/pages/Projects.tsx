64e96841765a311ed002eb2ddff148fc94433d1a
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-03 11:14:03 -0400

    Updated light mode styles

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,13 +3,17 @@ import { motion } from 'framer-motion';
 import { FilterByMainTechnology } from '../components/FilterByMainTechnologyDropdown';
 import PersonalProjects from '../components/PersonalProjects';
 import { MainTechnologyProvider } from '../context/MainTechnologyContext'
+import { useContext } from "react";
+import { RefContext } from "../context/ScrollContext";
+
 
 export default function Projects() {
+  const { portfolioRef } = useContext(RefContext);
   
   return (
     <>
     <MainTechnologyProvider>
-    <div className="flex flex-col justify-center items-center pb-4 pt-12" id='projects'>
+    <div className="flex flex-col justify-center items-center pb-4 pt-12" ref={portfolioRef}>
     <h3
             className="font-bold text-4xl lex text-left max-639:justify-center max-639:text-4xl main
      text-mainExtraDarkBlue dark:text-white lg:text-5xl"
