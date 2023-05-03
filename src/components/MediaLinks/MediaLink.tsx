2fd95a6d21d11360fe2c71f95a880af0c6d369ce
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-03 11:17:26 -0400

    Added title property

--- src/components/MediaLinks/MediaLink.tsx
+++ src/components/MediaLinks/MediaLink.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import { FooterCardProps as Footer } from "../../Interfaces/FooterCardProps";
 
-export default function FooterCard({socialLink, styleClass, icon}: Footer) {
+export default function FooterCard({socialLink, styleClass, icon, title}: Footer) {
     return(
         <React.Fragment>
-            <a href={socialLink}><button className={styleClass}>{icon}</button></a>
+            <a href={socialLink}><button className={styleClass}>{icon}{title}</button></a>
         </React.Fragment>
     )
 }
