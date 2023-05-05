31be22f6b1ecef038a96aee27618446917cb6f28
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-05 15:42:15 -0400

    Removed unnecesary TSX

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,8 +1,7 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useContext } from "react";
-import { RefContext } from "../context/ScrollContext";
+
 
 
 
