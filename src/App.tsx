4a785bbbe36fc8184df6a43de8df1e77ada04bd1
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-04 21:31:15 -0400

    Removed old routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import "./App.css";
 
 // Pages
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
 import RootLayout from "./layouts/RootLayout";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./context/DarkModeContext";
@@ -19,8 +17,6 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="projects" element={<Projects />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
