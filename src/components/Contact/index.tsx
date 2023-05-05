cd6c7bab3b2356c5abafa19d163c75753de25d6a
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-05 12:07:03 -0400

    Added reference for scrolling and button

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -3,16 +3,20 @@ import ContactForm from "../ContactForm"
 import { useContext } from "react";
 import { RefContext } from "../../context/ScrollContext";
 import Card from "./card"
+import { ScrollToComponent } from "../../hooks/ScrollToComponent";
+
 
 
 export default function Contact() {
-  const { contactRef } = useContext(RefContext);
+  const { emailRef } = useContext(RefContext);
     const mailerServiceID = import.meta.env.VITE_SERVICE_ID;
     const mailerTemplateID = import.meta.env.VITE_TEMPLATE_ID;
     const mailerUserID = import.meta.env.VITE_USER_ID;
 
+    const scrollToMailerRef = ScrollToComponent({ rootRef: emailRef });
+
     return(
-        <div className="py-20 w-full" ref={contactRef}>
+        <div className="py-20 w-full">
 
             <div className="flex flex-col justify-center items-center py-12">
               <h3
@@ -30,8 +34,14 @@ flex py-2 max-639:justify-center lg:text-3xl"
               <p className="dark:text-mainSilver/80 md:text-xl text-center">
                 Let's connect and create something together!
               </p>
+              <button
+                      onClick={scrollToMailerRef}
+                      className="dark:text-white py-4 text-mainExtraDarkBlue font-bold flex items-center gap-4"
+                    >
+                      Send me an emai
+                    </button>
             </div>
-            <p className="text-center text-mainSilver/50 py-2"></p>
+
             <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
             {contactSections.map((contact) => (
           <Card {...contact} key={contact.id} />
