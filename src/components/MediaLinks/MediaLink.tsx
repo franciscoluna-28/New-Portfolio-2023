bcf406044523c1d959eb6754d54cf503d88c9c1a
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-04 21:38:44 -0400

    Removed old title property

--- src/components/MediaLinks/MediaLink.tsx
+++ src/components/MediaLinks/MediaLink.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import { FooterCardProps as Footer } from "../../Interfaces/FooterCardProps";
 
-export default function FooterCard({socialLink, styleClass, icon, title}: Footer) {
+export default function FooterCard({socialLink, styleClass, icon}: Footer) {
     return(
         <React.Fragment>
-            <a href={socialLink}><button className={styleClass}>{icon}{title}</button></a>
+            <a href={socialLink}><button className={styleClass}>{icon}</button></a>
         </React.Fragment>
     )
 }
