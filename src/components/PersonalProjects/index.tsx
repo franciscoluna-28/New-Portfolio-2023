fd0c416811d19ef770adef845a0bc088f4b17659
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-04 21:37:54 -0400

    Added key to the project props

--- src/components/PersonalProjects/index.tsx
+++ src/components/PersonalProjects/index.tsx
@@ -1,4 +1,4 @@
-import { createdProjects } from "./data";
+import { createdProjects } from "../../data/projectDatabase";
 import ProjectCard, { project } from "../ProjectCard";
 import { useContext } from "react";
 import { MainTechnologyContext } from "../../context/MainTechnologyContext";
@@ -18,14 +18,16 @@ export default function PersonalProjects() {
 
   return (
     <div className="m-auto w-full flex gap-4 lg:gap-10 justify-center flex-wrap mt-6">
-        {filteredProjects.map((project) => (          <motion.div
+        {filteredProjects.map((project) => (          
+        <motion.div
             viewport={{ once: false }}
             initial={{ opacity: 0, x: 50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ type: "spring", duration: 1, delay: 0.15 }}
+            key={project.id}
           >
 
-            <ProjectCard {...project} />
+            <ProjectCard {...project} key={project.id} />
             </motion.div>
         ))}
 
