2039ebd8d8ace5dc78dcb3e838bc9834e0081ed7
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-04 21:35:27 -0400

    Updated screen

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,25 +1,30 @@
-import errorImage from "../assets/404.svg"
 import "../utils/glowStyles.css";
 import { motion } from 'framer-motion';
+import NavigationButton from "../components/Buttons/NavigationButton";
 
 
 
 export default function NotFound() {
 
     return (
+        <main className="h-screen flex justify-center items-center">
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}>
             <section className="p-8">
                 <h2 className="font-bold text-4xl text-center max-639:text-4xl main
-           text-mainDarkCyan lg:text-5xl dark:text-mainCyan">Not Found!</h2>
-                <p className="text-silver leading-loose py-2 w-auto max-639:w-72 max-639:m-auto max-639:text-center text-xl dark:text-mainSilver
-            text-mainDarkBlue text-center">Whoops! Looks like the page you were trying to reach is not available.
-                    Please try a different link or navigate to the <a
-                        className="font-bold outline-2 hover:underline hover:text-white" href="/">Homepage</a>.</p>
-                <img className="m-auto max-w-lg error-image dark:hidden" src={errorImage} alt="404 error"></img>
+           text-mainExtraDarkBlue lg:text-5xl dark:text-white">Not Found!</h2>
+           <h3 className="text-center text-mainDarkCyan font-bold dark:text-mainCyan text-4xl pt-2">404</h3>
+                <p className="dark:text-silver leading-loose w-auto max-639:w-72 max-639:m-auto max-639:text-center text-xl dark:text-mainSilver
+            text-mainDarkBlue/70 text-center py-4">Whoops! Looks like the page you were trying to reach is not available.</p>
+                            <NavigationButton
+                  text={"Home"}
+                  route={"/"}
+                  styleClass={"primary-button mb-4"}
+                />
             </section>
         </motion.div>
+        </main>
     )
 }
